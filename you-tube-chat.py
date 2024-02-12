6bba07f5b875589588d8a7b90ba8fda6c65cbaa6
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2024-02-12 04:35:09 +0000

    Update you-tube-chat.py

--- you-tube-chat.py
+++ you-tube-chat.py
@@ -10,6 +10,7 @@ from llama_index import VectorStoreIndex, load_index_from_storage, ServiceContex
 #from llama_hub.youtube_transcript import YoutubeTranscriptReader
 #from llama_index.readers import YoutubeTranscriptReader
 from llama_hub.youtube_transcript import YoutubeTranscriptReader
+from llama_hub.youtube_transcript import is_youtube_video
 
 load_dotenv()
 
@@ -18,23 +19,12 @@ storage_path = "./vectorstore"
 llm = OpenAI(temperature=0.1, model="gpt-4-turbo-preview")
 service_context = ServiceContext.from_defaults(llm=llm)
 
+documents = None
 
-# with st.sidebar:
-#     st.title("Youtube")
-#     question_text = st.text_area(label="Youtube URL")
-
-# with st.sidebar:
-#     st.button(label="Load URL", on_click=lambda: loadYoutubeURL())
-    
-
-url="https://www.youtube.com/watch?v=NBIRzSGHFro"
-
-loader = YoutubeTranscriptReader()
-documents = loader.load_data(ytlinks=[url])
-
-index = VectorStoreIndex.from_documents(documents)
-index.storage_context.persist(persist_dir=storage_path)    
-
+with st.sidebar:
+    st.title("Youtube")
+    urlTextValue = st.text_input(label="Youtube URL")
+    st.button(label="Load URL", on_click=lambda: loadYoutubeURL(urlTextValue))
 
 st.title("Ask the Youtube")
 if "messages" not in st.session_state.keys(): 
@@ -42,7 +32,35 @@ if "messages" not in st.session_state.keys():
         {"role": "assistant", "content": "Ask me a question !"}
     ]
 
-chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+
+def cleanIndex():
+    global documents
+    if documents != None :
+        st.write("Found Index and Vectors, Cleaning up now...")
+        ids = [str(i) for i in range(1, len(docs) + 1)]
+        index = VectorStoreIndex.from_documents(documents)
+        docsToDelete = index.similarity_search("")
+        print(docsToDelete[0].metadata)
+        st.write("Count before cleanup", documents._collection.count())
+        docsToDelete._collection.delete(ids=[ids[-1]])
+        st.write("count after cleanup", documents._collection.count())
+
+def loadYoutubeURL(url):
+    global documents
+    cleanIndex()
+    if is_youtube_video(url) == True :
+        with st.spinner("Loading the Index..."):
+            print(url)
+            loader = YoutubeTranscriptReader()
+            documents = loader.load_data(ytlinks=[url])
+            index = VectorStoreIndex.from_documents(documents)
+            index.storage_context.persist(persist_dir=storage_path)
+            chat_engine = index.as_chat_engine(chat_mode="condense_question", streaming=True, verbose=True)
+            print(chat_engine)
+            st.session_state["chat_engine"] = chat_engine
+    else :
+        st.error("Please check the youtube URL, it doesn't seem to be valid", icon="🚨")
+
 
 if prompt := st.chat_input("Your question"): 
     st.session_state.messages.append({"role": "user", "content": prompt})
@@ -50,12 +68,18 @@ if prompt := st.chat_input("Your question"):
 for message in st.session_state.messages: 
     with st.chat_message(message["role"]):
         st.write(message["content"])
-        
+
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = chat_engine.chat(prompt)
-            st.write(response.response)
-            pprint_response(response, show_source=True)
-            message = {"role": "assistant", "content": response.response}
-            st.session_state.messages.append(message) 
+            print("Prompt recieved")
+            chat_engine = st.session_state["chat_engine"]
+            print(chat_engine)
+            if chat_engine != None :
+                response = chat_engine.chat(prompt)
+                st.write(response.response)
+                pprint_response(response, show_source=True)
+                message = {"role": "assistant", "content": response.response}
+                st.session_state.messages.append(message) 
+            else :
+                st.write("Please load a youtube video first...")
